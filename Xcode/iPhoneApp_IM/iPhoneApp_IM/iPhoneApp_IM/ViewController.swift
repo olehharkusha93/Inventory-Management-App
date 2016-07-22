8796cab1eb8831bec976bf1ae8ad9e5adb194179
brandondbigelow12 <brandondbigelow12@gmail.com>
2016-07-22 16:25:13 -0400

    Update Xcode Files

--- Xcode/iPhoneApp_IM/iPhoneApp_IM/iPhoneApp_IM/ViewController.swift
+++ Xcode/iPhoneApp_IM/iPhoneApp_IM/iPhoneApp_IM/ViewController.swift
@@ -13,12 +13,22 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var usernameTextField: UITextField?
     @IBOutlet weak var passwordTextField: UITextField?
+    
+    
+    /////////////////////////////
+    
+//    @IBOutlet weak var usernameTextField : UITextField? = UITextField()
+//    @IBOutlet weak var passwordTextField : UITextField? = UITextField()
+    
+    
+    /////////////////////
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // Do any additional setup after loading the view, typically from a nib.
-        self.usernameTextField?.text = "@Test_User1"
-        self.passwordTextField?.text = "password"
+        self.usernameTextField?.text = ""
+        self.passwordTextField?.text = ""
         
     }
     
@@ -35,16 +45,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func loginTapped(sender: UIButton) {
-        print("hi")
+//   @IBAction func loginTapped(sender: UIButton)
+//   {
+//    
+//        self.performSegueWithIdentifier("goTo_Inventory", sender: nil)
+//    
+//   }
+    
+    @IBAction func loginTapped(sender : UIButton) {
         
-        if self.usernameTextField?.text == "Dave" {
+        if (usernameTextField!.text == ""  || passwordTextField!.text == "")
+        {
+            print("Sign in failed. Empty characters")
+        }
+        else if(usernameTextField?.text == "test" && passwordTextField?.text == "password")
+        {
             self.performSegueWithIdentifier("goTo_Inventory", sender: nil)
         }
-        
+        else {
+            print("You shouldn't see this")
+        }
     }
+
+    
     
     @IBAction func clearTapped(sender: UIButton) {
+       
+        self.usernameTextField?.text = "";
+        self.passwordTextField?.text = "";
         
     }
 
