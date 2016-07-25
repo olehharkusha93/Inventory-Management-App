ab0f3b3a6b8d086705ae39ac21c01bd6a8273df8
Gabriel Figueiredo B. Leo <gabrielboneragp@hotmail.com>
2016-07-25 13:40:46 -0400

    Fixing push...

---
+++ Win_InvApp/Win_InvApp/Barcode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnBarcode.Barcode.BarcodeScanner;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Win_InvApp
+{
+    class Barcode
+    {
+        public Barcode()
+        {
+
+        }
+
+        public String[] Scan()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "All Files|*.*|jpg Files|*.png";
+            dlg.FilterIndex = 2;
+            string[] barcodes = { "DEFAULT" };
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                barcodes = BarcodeScanner.Scan(dlg.FileName, BarcodeType.Code128);
+                return barcodes;
+            }
+            else
+            {
+                return barcodes;
+            }
+        }
+        
+    }
+}
