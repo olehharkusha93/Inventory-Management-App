3262356bb28f4b42e5bd12e9509f2ac0489eddc8
Gabriel Figueiredo B. Leo <gabrielboneragp@hotmail.com>
2016-08-03 19:12:14 -0400

    Added Android project

---
+++ Android/Android_InvManagement/app/src/main/java/domain/android_invmanagement/WelcomeScreen.java
@@ -0,0 +1,13 @@
+package domain.android_invmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
