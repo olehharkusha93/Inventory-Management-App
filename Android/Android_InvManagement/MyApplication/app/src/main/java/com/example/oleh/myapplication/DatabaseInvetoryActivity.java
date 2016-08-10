25b38b528455703811995c2607c59c819ffe4c93
Gabriel Figueiredo B. Leo <gabrielboneragp@hotmail.com>
2016-08-10 16:29:20 -0400

    Commit of 08/10 - Worked on Inv screen

--- Android/Android_InvManagement/MyApplication/app/src/main/java/com/example/oleh/myapplication/DatabaseInvetoryActivity.java
+++ Android/Android_InvManagement/MyApplication/app/src/main/java/com/example/oleh/myapplication/DatabaseInvetoryActivity.java
@@ -2,16 +2,22 @@ package com.example.oleh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class DatabaseInvetoryActivity extends AppCompatActivity {
     TextView text;
+    ListView list;
+    SearchView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database_invetory);
 
-        text = (TextView)findViewById(R.id.testText);
+        text = (TextView)findViewById(R.id.inventoryLabel);
+        //search = (SearchView)findViewById(R.id.searchView);
+        //list = (ListView)findViewById(R.id.invList);
     }
 }
