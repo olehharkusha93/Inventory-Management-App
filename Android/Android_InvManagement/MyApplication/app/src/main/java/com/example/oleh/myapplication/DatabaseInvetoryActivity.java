b7f8f37381a5909c66a0ef143726b483306b6f60
Gabriel Figueiredo B. Leo <gabrielboneragp@hotmail.com>
2016-08-20 21:05:41 -0400

    Scan button in Inventory implemented

--- Android/Android_InvManagement/MyApplication/app/src/main/java/com/example/oleh/myapplication/DatabaseInvetoryActivity.java
+++ Android/Android_InvManagement/MyApplication/app/src/main/java/com/example/oleh/myapplication/DatabaseInvetoryActivity.java
@@ -2,6 +2,7 @@ package com.example.oleh.myapplication;
 
 import android.app.ListActivity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.support.v7.widget.DecorToolbar;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,6 +26,7 @@ public class DatabaseInvetoryActivity extends AppCompatActivity {
     String[] food = {"Corn", "Potato", "Tomato"};
     CloudQuery itemList;
     ArrayAdapter<String> adapter;
+    Button scan;
     public ProgressDialog pdialog;
 
 
@@ -33,14 +36,17 @@ public class DatabaseInvetoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_database_invetory);
         pdialog=new ProgressDialog(this);
 
-        text = (TextView) findViewById(R.id.testText);
+        //text = (TextView) findViewById(R.id.testText);
         listView = (ListView)findViewById(R.id.listView);
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,food);
         //adapter = new ArrayAdapter<String>(this,q, itemList);
         listView.setAdapter(adapter);
+        scan = (Button)findViewById(R.id.scanActivityBtn);
         // search = (SearchView)findViewById(R.id.searchView);
         //new Query().execute();
 
+        Intent Scan_pg = new Intent(DatabaseInvetoryActivity.this, BarcodeScan.class);
+        DatabaseInvetoryActivity.this.startActivity(Scan_pg);
     }
 
      public class Query extends AsyncTask<String, String, String> {
